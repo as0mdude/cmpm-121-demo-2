7eb87d18fd3f34bf87e36892b8aa27e8b24d95e5
asom <86374869+as0mdude@users.noreply.github.com>
2024-10-16 12:34:43 -0700

    small change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "This is a small change.";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
