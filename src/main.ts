93f85ed7558331b1075426a40bf98cbc786d7959
asom <86374869+as0mdude@users.noreply.github.com>
2024-10-18 12:40:48 -0700

    step 1 done

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import "./style.css";
 
-const APP_NAME = "Retry";
+const APP_NAME = "My app";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement('h1');
+title.textContent = 'My Cool App';
+document.body.appendChild(title);
+
+
+
+// Create a canvas element
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = 'myCanvas';  // Add an ID to reference in CSS
+document.body.appendChild(canvas);
